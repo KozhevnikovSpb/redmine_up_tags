acce090ee34114b025ca40c0a68ad6fcc39ad1c8
KozhevnikovSPB <dev@kozhevnikovspb.ru>
2026-07-10 18:37:25 +0300

    Add new settings page

--- app/models/tag_cloud.rb
+++ app/models/tag_cloud.rb
@@ -2,10 +2,10 @@ class TagCloud < ActiveRecord::Base
   belongs_to :project
   belongs_to :created_by, class_name: 'User'
 
-  # Новый синтаксис для Rails 8
-  serialize :status_filter, coder: Array
-  serialize :version_filter, coder: Array
-  serialize :tracker_filter, coder: Array
+  # Правильный синтаксис для Rails 8
+  serialize :status_filter, Array
+  serialize :version_filter, Array
+  serialize :tracker_filter, Array
 
   validates :name, presence: true, uniqueness: { scope: :project_id }
   validates :project, presence: true
