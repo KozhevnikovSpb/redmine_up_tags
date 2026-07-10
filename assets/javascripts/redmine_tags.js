f67ce39e566131ba876af0de67f99ffbf8f59564
KozhevnikovSPB <57673312+KozhevnikovSpb@users.noreply.github.com>
2026-07-10 22:33:56 +0300

    fix: stabilize tag permissions, queries and multi-clouds

--- assets/javascripts/redmine_tags.js
+++ assets/javascripts/redmine_tags.js
@@ -1,19 +1,21 @@
 $(function () {
     $('body').on('click', '.most_used_tags .most_used_tag', function (e) {
         var $tagsSelect = $('select#issue_tag_list');
-        var tag = e.target.innerText;
-        if ($tagsSelect.find("option[value='" + tag + "']").length === 0) {
+        var tag = $(e.currentTarget).text();
+
+        if ($tagsSelect.find('option').filter(function () { return $(this).val() === tag; }).length === 0) {
             var newOption = new Option(tag, tag, true, true);
-            $tagsSelect.append(newOption).trigger("change");
+            $tagsSelect.append(newOption).trigger('change');
         }
 
-        mostUsedTags = $.grep(mostUsedTags, function(t) { return t != tag; });
-        var tagsHtml = mostUsedTags.map(function(tag) {
-            return '<span class="most_used_tag">' + tag + '</span>';
-        }).join(', ');
+        window.mostUsedTags = $.grep(window.mostUsedTags || [], function (item) { return item !== tag; });
+        var $container = $(e.currentTarget).parent('.most_used_tags').empty();
 
-        var $mostUsedTagsContainer = $(e.target).parent('.most_used_tags');
-        $mostUsedTagsContainer.empty();
-        $mostUsedTagsContainer.append(tagsHtml);
+        $.each(window.mostUsedTags, function (index, item) {
+            if (index > 0) {
+                $container.append(document.createTextNode(', '));
+            }
+            $('<span>', { class: 'most_used_tag', text: item }).appendTo($container);
+        });
     });
 });
--- init.rb
+++ init.rb
@@ -1,27 +1,8 @@
-# This file is a part of Redmine Tags (redmine_tags) plugin,
-# customer relationship management plugin for Redmine
-#
-# Copyright (C) 2011-2026 RedmineUP
-# http://www.redmineup.com/
-#
-# redmine_tags is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# redmine_tags is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with redmine_tags.  If not, see <http://www.gnu.org/licenses/>.
-
 requires_redmineup version_or_higher: '1.1.10' rescue raise "\n\033[31mRedmine requires newer redmineup gem version.\nPlease update with 'bundle update redmineup'.\033[0m"
 
 require 'redmine'
 
-TAGS_VERSION_NUMBER = '2.1.2'
+TAGS_VERSION_NUMBER = '2.1.3'
 TAGS_VERSION_TYPE = 'Light version'
 
 Redmine::Plugin.register :redmineup_tags do
@@ -32,7 +13,7 @@ Redmine::Plugin.register :redmineup_tags do
   url 'https://www.redmineup.com/pages/plugins/tags/'
   author_url 'mailto:support@redmineup.com'
 
-  requires_redmine version_or_higher: '4.0'
+  requires_redmine version_or_higher: '7.0'
 
   settings default: {
     sidebar_tag_list_view: 'none',
@@ -44,20 +25,28 @@ Redmine::Plugin.register :redmineup_tags do
     tags_suggestion_order: 'name'
   }, partial: 'tags/settings'
 
-  # Permissions for Tag Clouds
+  project_module :issue_tracking do
+    permission :create_tags, {}
+    permission :edit_tags, {}
+  end
+
   project_module :tags do
     permission :manage_tag_clouds, {
-      tag_clouds: [:index, :new, :create, :edit, :update, :destroy]
+      tag_clouds: %i[index new create edit update destroy reorder]
     }, require: :member
     permission :select_tag_clouds, {
-      tag_clouds: [:toggle_visibility]
+      tag_cloud_preferences: [:toggle]
     }
   end
 
-  menu :admin_menu, :tags, { controller: 'settings', action: 'plugin', id: 'redmineup_tags' }, caption: :tags, html: { class: 'icon' }, icon: 'tag', plugin: :redmineup_tags
+  menu :admin_menu, :tags,
+       { controller: 'settings', action: 'plugin', id: 'redmineup_tags' },
+       caption: :tags,
+       html: { class: 'icon' },
+       icon: 'tag',
+       plugin: :redmineup_tags
 end
 
-# Patch for adding tab to Project Settings
 module RedmineupTags
   module Patches
     module ProjectsHelperPatch
@@ -70,7 +59,8 @@ module RedmineupTags
             unless tabs.any? { |tab| tab[:name] == 'tags' }
               tabs << {
                 name: 'tags',
-                action: :plugin,
+                action: :manage_tag_clouds,
+                module: :tags,
                 partial: 'projects/settings/tags',
                 label: :tag_clouds
               }
@@ -83,7 +73,6 @@ module RedmineupTags
   end
 end
 
-# Apply patch
 unless ProjectsHelper.included_modules.include?(RedmineupTags::Patches::ProjectsHelperPatch)
   ProjectsHelper.include RedmineupTags::Patches::ProjectsHelperPatch
 end
