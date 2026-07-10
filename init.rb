f4f93ef4d76e355bf1f101a42d51d41b34787aea
KozhevnikovSPB <dev@kozhevnikovspb.ru>
2026-07-10 17:24:21 +0300

    Add new settings page

--- init.rb
+++ init.rb
@@ -1,3 +1,29 @@
+# This file is a part of Redmine Tags (redmine_tags) plugin,
+# customer relationship management plugin for Redmine
+#
+# Copyright (C) 2011-2026 RedmineUP
+# http://www.redmineup.com/
+#
+# redmine_tags is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# redmine_tags is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with redmine_tags.  If not, see <http://www.gnu.org/licenses/>.
+
+requires_redmineup version_or_higher: '1.1.10' rescue raise "\n\033[31mRedmine requires newer redmineup gem version.\nPlease update with 'bundle update redmineup'.\033[0m"
+
+require 'redmine'
+
+TAGS_VERSION_NUMBER = '2.1.2'
+TAGS_VERSION_TYPE = 'Light version'
+
 Redmine::Plugin.register :redmineup_tags do
   name "Redmine Tags plugin (#{TAGS_VERSION_TYPE})"
   author 'RedmineUP'
@@ -29,4 +55,13 @@ Redmine::Plugin.register :redmineup_tags do
   end
 
   menu :admin_menu, :tags, { controller: 'settings', action: 'plugin', id: 'redmineup_tags' }, caption: :tags, html: { class: 'icon' }, icon: 'tag', plugin: :redmineup_tags
+end
+
+if Rails.configuration.respond_to?(:autoloader) && Rails.configuration.autoloader == :zeitwerk
+  Rails.autoloaders.each { |loader| loader.ignore(File.dirname(__FILE__) + '/lib') }
+end
+require File.dirname(__FILE__) + '/lib/redmineup_tags'
+
+ActiveSupport.on_load(:action_view) do
+  include TagsHelper
 end
